80bcb39a738a01b9ce4835dc7b65e0f4d75857f6
Jirka Balhar <balhar.j@gmail.com>
2023-12-07 22:04:16 +0100

    download python scripts

---
+++ 00_download_data.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+
+# download only the first archive
+dataset = load_dataset(
+    "codeparrot/codeparrot-clean-train", data_files="file-000000000001.json.gz"
+)
+
+print(dataset)
+
+for sample in dataset["train"]:
+    # todo: shard the data according to the gz archives
+    with open(
+        f"../data/codeparrot-clean-train/original_code/{sample['hash']}.py", "w"
+    ) as f:
+        f.write(sample["content"])
+
+    # py_compile.compile('data/codeparrot-clean-train/test.py', 'data/codeparrot-clean-train/test.pyc')
